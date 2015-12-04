8af0ecbe226c1f983a9b609784bfd1542ebcd136
RizOrt218 <rizhiafalen@gmail.com>
2015-12-03 21:46:27 -1000

    vaultaraaa

--- vault.js
+++ vault.js
@@ -1,4 +1,27 @@
-'use strict';
 module.exports = function() {
-  
+  'use strict';
+
+  var set_value;
+  var set_key;
+
+  return {
+    getValue: function(key){
+      if (key === undefined){
+        return null;
+      }
+      else if (key === set_key){
+        return set_value;
+      }
+      else if (key !== set_key){
+        return null;
+      }
+    },
+    setValue : function(key, value){
+        set_key = key;
+        set_value = value;
+    }
+    };
+
 };
+
+
